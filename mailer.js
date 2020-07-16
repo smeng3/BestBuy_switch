942238539cef7d6c52bbfccf883168d830f44b44
smeng3 <47490065+smeng3@users.noreply.github.com>
2020-07-16 00:45:22 -0400

    Update mailer.js

--- mailer.js
+++ mailer.js
@@ -3,16 +3,16 @@ const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
   service: 'Gmail',
   auth: {
-    user: 'switchlqy@gmail.com',
-    pass: 'lqyswitch1021'
+    user: 'test@gmail.com',
+    pass: '******'
   }
 });
 
 const send = async (text) => {
   await transporter.sendMail({
-    from: '"switchlqy" <switchlqy@gmail.com>', // sender address
-    to: "mengsixu@gmail.com, qliu23@u.rochester.edu", // list of receivers
-    subject: "你的Switch有货了", // Subject line
+    from: '"test" <test@gmail.com>', // sender address
+    to: "****@gmail.com", // list of receivers
+    subject: " Switch is availabe", // Subject line
     html: text // html body
   });
 };
